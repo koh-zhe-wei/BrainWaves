feb51c066118e6baec6120dabaa7f6178b78da0c
koh-zhe-wei <k.zhewei@u.nus.edu>
2022-07-16 10:05:44 +0800

    initialised chatlist

---
+++ components/ChatList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, SafeAreaView, Text, TouchableOpacity, Image} from "react-native"
+import tw from "tailwind-rn";
+
+
+
+const ChatList = () => {
+    return (
+        <View>
+            <Text>Chatlist...</Text>
+        </View>
+    )
+}
+
+export default ChatList;
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, SafeAreaView, Text, TouchableOpacity, Image} from "react-native"
 import Header from "../components/Header";
+import ChatList from "../components/ChatList";
 
 
 const ChatScreen = () => {
     return (
         <SafeAreaView>
             <Header title="Chat"/>
+            <ChatList />
         </SafeAreaView>
     )
 }
